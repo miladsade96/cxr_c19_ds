f4792c1abc510330f752671490008040876cbfa5
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-08-03 23:36:24 +0430

    Terminating training process when validation accuracy reaches the baseline

--- cxr_c19_ds/main.py
+++ cxr_c19_ds/main.py
@@ -72,6 +72,26 @@ model.compile(
 # Model architecture summary
 model.summary()
 
+
+class TerminateOnBaseLine(Callback):
+    """Callback that terminates training when either acc or val_acc reaches a specified baseline"""
+    def __init__(self, monitor='accuracy', baseline=0.9):
+        super(TerminateOnBaseLine, self).__init__()
+        self.monitor = monitor
+        self.baseline = baseline
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        acc = logs.get(self.monitor)
+        if acc is not None:
+            if acc >= self.baseline:
+                print('Epoch %d: Reached baseline, terminating training' % epoch)
+                self.model.stop_training = True
+
+
+# Terminating training process when validation accuracy reaches the specified baseline
+cb = [TerminateOnBaseLine(monitor='val_accuracy', baseline=0.97)]
+
 # Training section
 history = model.fit_generator(
     train_data,
