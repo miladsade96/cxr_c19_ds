d32a144178d255bbaa7aabc87582321af5011a19
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-08-06 12:16:45 +0430

    Setting dropout rate to 0.2

--- cxr_c19_ds/main.py
+++ cxr_c19_ds/main.py
@@ -23,7 +23,7 @@ for layer in vgg_model.layers[:17]:
 output = vgg_model.output
 output = Flatten()(output)
 output = Dense(512, activation="relu")(output)
-output = Dropout(0.5)(output)
+output = Dropout(0.2)(output)
 output = Dense(3, activation="softmax")(output)
 model = Model(inputs=vgg_model.inputs, outputs=output)
 
