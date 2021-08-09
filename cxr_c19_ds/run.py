d3d078e94968b7870db0a73252245df83b75cd58
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-08-09 23:56:51 +0430

    Optimizing import statements

--- cxr_c19_ds/run.py
+++ cxr_c19_ds/run.py
@@ -4,9 +4,6 @@
 
 
 import numpy as np
-from PIL import Image
-import streamlit as st
-from tensorflow import expand_dims
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
