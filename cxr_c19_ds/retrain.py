cd530b0f837a949e90245192c503e84e97606bc4
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-08-11 20:22:35 +0430

    Setting image data generator parameters

--- cxr_c19_ds/retrain.py
+++ cxr_c19_ds/retrain.py
@@ -4,3 +4,16 @@
 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+
+
+# Setting image data generator parameters
+# These are will used for image augmentation
+data_gen_args = dict(
+    rotation_range=0.2,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True,
+    vertical_flip=True,
+    rescale=1./255,
+    zoom_range=0.3,
+)
