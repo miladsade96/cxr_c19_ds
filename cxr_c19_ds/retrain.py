2555843b65191eb8fcd961330d3b3f192127d2ae
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-08-11 20:26:25 +0430

    Callback that terminates training when either acc or val_acc reaches a specified baseline

--- cxr_c19_ds/retrain.py
+++ cxr_c19_ds/retrain.py
@@ -4,6 +4,7 @@
 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from tensorflow.python.keras.callbacks import Callback
 
 
 # Setting image data generator parameters
@@ -27,3 +28,19 @@ train_data = data_gen.flow_from_directory(
     batch_size=32,
     subset="training"
 )
+
+
+class TerminateOnBaseLine(Callback):
+    """Callback that terminates training when either acc or val_acc reaches a specified baseline"""
+    def __init__(self, monitor='accuracy', baseline=0.9):
+        super(TerminateOnBaseLine, self).__init__()
+        self.monitor = monitor
+        self.baseline = baseline
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        acc = logs.get(self.monitor)
+        if acc is not None:
+            if acc >= self.baseline:
+                print('Epoch %d: Reached baseline, terminating training' % epoch)
+                self.model.stop_training = True
