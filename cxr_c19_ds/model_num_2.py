4083cf0d9c06abe26509e020660bdbf0caf40b55
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-09-09 23:17:11 +0430

    Importing relevant modules

--- cxr_c19_ds/model_num_2.py
+++ cxr_c19_ds/model_num_2.py
@@ -3,3 +3,12 @@
 
     Author: Milad Sadeghi DM - EverLookNeverSee@GitHub
 """
+
+
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.activations import relu, softmax
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.callbacks import ModelCheckpoint, Callback
+from tensorflow.keras.layers import (Conv2D, Dense, BatchNormalization, Concatenate,
+                                     Flatten, MaxPooling2D, AveragePooling2D)
