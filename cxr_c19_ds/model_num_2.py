d99fe36af1d17a5925020e535cc78c3b5d7136cc
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-09-10 16:02:43 +0430

    Defining model input layer and first parallel layer

--- cxr_c19_ds/model_num_2.py
+++ cxr_c19_ds/model_num_2.py
@@ -13,3 +13,12 @@ from tensorflow.keras.losses import categorical_crossentropy
 from tensorflow.keras.callbacks import ModelCheckpoint, Callback
 from tensorflow.keras.layers import (Conv2D, Dense, BatchNormalization, Concatenate,
                                      Flatten, MaxPooling2D, AveragePooling2D, Input)
+
+
+# Defining model input
+input_ = Input(shape=(224, 224, 3))
+
+# Defining first parallel layer
+x = Conv2D(filters=16, kernel_size=(3, 3), activation=relu)(input_)
+x = BatchNormalization()(x)
+x = AveragePooling2D(pool_size=(2, 2), strides=(3, 3))(x)
