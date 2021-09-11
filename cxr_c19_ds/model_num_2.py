b26067f77958dec9951127e265041b860794d447
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-09-11 21:28:59 +0430

    Fixing model concatenation issues

--- cxr_c19_ds/model_num_2.py
+++ cxr_c19_ds/model_num_2.py
@@ -19,21 +19,21 @@ from tensorflow.keras.layers import (Conv2D, Dense, BatchNormalization, Concaten
 input_ = Input(shape=(224, 224, 3))
 
 # Defining first parallel layer
-in_1 = Conv2D(filters=16, kernel_size=(3, 3), activation=relu)(input_)
+in_1 = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(input_)
 conv_1 = BatchNormalization()(in_1)
 conv_1 = AveragePooling2D(pool_size=(2, 2), strides=(3, 3))(conv_1)
 
 # Defining second parallel layer
-in_2 = Conv2D(filters=16, kernel_size=(5, 5), activation=relu)(input_)
+in_2 = Conv2D(filters=16, kernel_size=(5, 5), activation='relu', padding='same')(input_)
 conv_2 = BatchNormalization()(in_2)
 conv_2 = AveragePooling2D(pool_size=(2, 2), strides=(3, 3))(conv_2)
 
 # Defining third parallel layer
-in_3 = Conv2D(filters=16, kernel_size=(5, 5), activation=relu)(input_)
+in_3 = Conv2D(filters=16, kernel_size=(5, 5), activation='relu', padding='same')(input_)
 conv_3 = BatchNormalization()(in_3)
 conv_3 = MaxPooling2D(pool_size=(2, 2), strides=(3, 3))(conv_3)
 
 # Defining fourth parallel layer
-in_4 = Conv2D(filters=16, kernel_size=(9, 9), activation=relu)(input_)
+in_4 = Conv2D(filters=16, kernel_size=(9, 9), activation='relu', padding='same')(input_)
 conv_4 = BatchNormalization()(in_4)
 conv_4 = MaxPooling2D(pool_size=(2, 2), strides=(3, 3))(conv_4)
