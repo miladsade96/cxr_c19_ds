8b21e6010bc3197458a68095bd9c026bbcbbb3e7
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-09-12 11:46:31 +0430

    Removing unnecessary parts

--- cxr_c19_ds/main.py
+++ cxr_c19_ds/main.py
@@ -12,22 +12,6 @@ from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.losses import categorical_crossentropy
 
 
-# Instantiating the model with passing default parameters values
-# Weights parameter may be either "imagenet" string or direct path to .h5 file downloaded from googleapis
-vgg_model = VGG16(weights="imagenet", include_top=False, input_shape=(224, 244, 3))
-
-# Freezing all vgg-16 model layers except last convolutional block
-for layer in vgg_model.layers[:15]:
-    layer.trainable = False
-
-# Adding flatten and dense layers to vgg-16
-output = vgg_model.output
-output = Flatten()(output)
-output = Dense(512, activation="relu")(output)
-output = Dropout(0.2)(output)
-output = Dense(4, activation="softmax")(output)
-model = Model(inputs=vgg_model.inputs, outputs=output)
-
 # Setting image data generator parameters
 # These are will used for image augmentation
 data_gen_args = dict(
