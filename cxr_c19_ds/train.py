41d6b3fa03ad635e962266782d8b14a343786d70
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-09-14 11:57:27 +0430

    Defining models dict

--- cxr_c19_ds/train.py
+++ cxr_c19_ds/train.py
@@ -9,3 +9,7 @@ from cxr_c19_ds.transferred_model import transferred_model
 from cxr_c19_ds.preprocessing import train_data, valid_data, cb
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.losses import categorical_crossentropy
+
+
+# Defining models
+models = {"VGG-16": transferred_model, "Parallel": parallel_model}
