b439300cf585719dc233b33a804887cbc46a8eb9
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-09-14 11:51:02 +0430

    Importing relevant modules

--- cxr_c19_ds/train.py
+++ cxr_c19_ds/train.py
@@ -3,3 +3,9 @@
 
     Author: Milad Sadeghi DM - EverLookNeverSee@GitHub
 """
+
+from cxr_c19_ds.parallel_model import parallel_model
+from cxr_c19_ds.transferred_model import transferred_model
+from cxr_c19_ds.preprocessing import train_data, valid_data, cb
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.losses import categorical_crossentropy
