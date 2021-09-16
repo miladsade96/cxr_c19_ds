41e5f6e26b76d8a584f3e0d8bb4e5600ffd3516f
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-09-17 00:22:04 +0430

    Instantiating argparse and adding arguments

--- cxr_c19_ds/demo.py
+++ cxr_c19_ds/demo.py
@@ -11,3 +11,12 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 # Defining class names
 classes = {0: "Covid", 1: "Lung Opacity", 2: "Normal", 3: "Viral Pneumonia"}
+
+# Instantiating cli argument parser
+parser = argparse.ArgumentParser()
+# Adding optional arguments
+parser.add_argument("-m", "--model", help="Path to saved model directory")
+parser.add_argument("-i", "--image", help="Path to image file")
+parser.add_argument("-v", "--verbose", action="store_true", help="Level of verbosity")
+# Parsing the arguments
+args = parser.parse_args()
